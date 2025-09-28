d069f456dfc91fd6e5c621953f8ed7cb54fa17a8
RajulThakur <gphotosbot@gmail.com>
2025-09-28 18:50:19 +0530

    imges chnges

--- src/main.js
+++ src/main.js
@@ -21,11 +21,10 @@ const bushHtml = `<div id="bush-container">${Array.from({ length: 6 }, (_, id) =
 
 const images = [
   { id: "finder", path: "/som/find.png" },
-  { id: "char", path: "/som/char.png" },
   { id: "gunMan", path: "/som/gun.png" },
   { id: "worker", path: "/som/worker.png" },
   { id: "sipper", path: "/som/sip.png" },
-  { id: "boat", path: "/som/boat.png" },
+  { id: "surfer", path: "/som/surfer.png" },
   { id: "knight", path: "/som/knight.png" },
   { id: "fisher", path: "/som/fisher.png" },
   { id: "newspaper", path: "/som/newspaper.png" },
@@ -38,55 +37,48 @@ const images = [
 ];
 // Dialogue system configuration
 const dialogueConfig = {
+  
   thief: {
     text: "I have the map! Want to see the treasure location?",
-    position: { top: "-150px", left: "50%" },
+    position: { top: "-7.375rem", right: "-1rem" },
     bubbleColor: "#FF0066",
   },
   finder: {
     text: "I found something interesting over here!",
-    position: { top: "-150px", left: "50%" },
+    position: { top: "-7.375rem", right: "-1rem" },
     bubbleColor: "#00AA66",
-  },
-  char: {
-    text: "Hello there! I'm the main character.",
-    position: { top: "-150px", left: "50%" },
-    bubbleColor: "#FF6600",
   },
   gunMan: {
     text: "Don't mess with me! I've got protection!",
-    position: { top: "-150px", left: "50%" },
+    position: { top: "-7.375rem", right: "0" },
     bubbleColor: "#CC0000",
   },
   worker: {
     text: "I'm working hard to build something great!",
-    position: { top: "-150px", left: "50%" },
+    position: { top: "-6.375rem", right: "0" },
     bubbleColor: "#0066CC",
   },
   sipper: {
     text: "This drink is absolutely delicious!",
-    position: { top: "-150px", left: "50%" },
+    position: { top: "-6.375rem", right: "0" },
     bubbleColor: "#9900CC",
   },
-  boat: {
+  surfer: {
     text: "Ready to sail the seven seas!",
-    position: { top: "-150px", left: "50%" },
+    position: { top: "-7.375rem", right: "0" },
     bubbleColor: "#006699",
   },
 };
 
-// Create dialogue bubble HTMLjhjhkhjdsfsdset
+// Create dialogue bubble HTML
 function createDialogueBubble(id, config) {
   return `
     <div class="dialogue-bubble" id="${id}-bubble" style="
       top: ${config.position.top};
-      left: ${config.position.left};
-      transform: translateX(-50%);
+      right: ${config.position.right};
+      transform: translateX(59%);
     ">
-      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-        <path fill="${config.bubbleColor}" d="M23.1,-33C32.8,-34.3,45.5,-34,56.1,-28.2C66.7,-22.4,75.2,-11.2,78,1.6C80.8,14.4,77.8,28.8,70.9,41C63.9,53.1,53,62.9,40.5,72C28,81.1,14,89.5,1.4,87.1C-11.3,84.8,-22.5,71.7,-35.3,62.7C-48.1,53.8,-62.4,49,-67,39.3C-71.7,29.5,-66.7,14.8,-68.3,-1C-70,-16.7,-78.3,-33.3,-73.2,-42.4C-68.2,-51.5,-49.8,-53,-35.4,-48.9C-21,-44.8,-10.5,-35.1,-1.9,-31.8C6.7,-28.5,13.3,-31.6,23.1,-33Z" transform="translate(100 100)" />
-      </svg>
-      <div class="dialogue-text">${config.text}</div>
+   ${config.text}
     </div>
   `;
 }
@@ -209,7 +201,9 @@ setTimeout(() => {
   // Show main content
   setTimeout(() => {
     document.querySelector("#app").innerHTML = `${html} ${imgHTML} ${bushHtml}`;
-    document.getElementById("thief").addEventListener('click',handleThiefClick)
+    document
+      .getElementById("thief")
+      .addEventListener("click", handleThiefClick);
     setupDialogueSystem();
   }, 500); // Wait for fade out animation
 }, 2000); // Show loading for 2 seconds
